aab1d15a4296109b4d25a0d82ae2c11a3b0182b0
kimquynh <quynh.nguyenkim1304@hcmut.edu.vn>
2023-10-23 08:12:29 +0700

    add lab1 and lab2

--- Lab01/pytest_hello_world.py
+++ Lab01/pytest_hello_world.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: CC0-1.0
 
-import hashlib
+import hashlib   
 import logging
 from typing import Callable
 
